d97552a4c60224ac6174603c80aab1bd1576c78f
sejaldhanve <sejaldhanve1805@gmail.com>
2024-12-20 16:33:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Routes } from 'react-router-dom'; // You don't need Router here
 import About from './pages/About';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import JobSearch from './pages/JobSearch';
+import Profile from './pages/ProfilePage';
+import Chat from './pages/Chat';
 import Footer from "./MyComponents/Footer.js";
 
 function App() {
@@ -15,9 +18,11 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/jobSearch" element={<JobSearch />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
 
       </Routes>
-      <Footer/>
       
     </>
   );
